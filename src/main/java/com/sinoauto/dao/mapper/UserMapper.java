5201f5c08d08d5f368046b3867b4b5e8f5026b37
tang <tang@DESKTOP-R8A6IOT>
2017-08-17 09:38:26 +0800

    增加登录接口

--- src/main/java/com/sinoauto/dao/mapper/UserMapper.java
+++ src/main/java/com/sinoauto/dao/mapper/UserMapper.java
@@ -1,5 +1,14 @@
 package com.sinoauto.dao.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.sinoauto.dao.bean.HqlsUser;
+
+@Mapper
 public interface UserMapper {
+	
+	@Select("select * from hqls_user where global_user_id=#{1} and is_useable = 1")
+	public HqlsUser getUserByGloabUserId(Integer gloabUserId);
 
 }
