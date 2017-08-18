f3fad8ab4184379e3047652e85a94ddeabeca339
tang <tang@DESKTOP-R8A6IOT>
2017-08-18 14:33:44 +0800

    中台分页

--- src/main/resources/static/hqls/common/index.js
+++ src/main/resources/static/hqls/common/index.js
@@ -1,5 +1,5 @@
 layui.use(['layer', 'jquery', 'element', 'laydate'], function() {
-    var url;
+    var url="Configuration/ServiceOrderConfiguration/ServiceOrderConfiguration.html";
     if (window.sessionStorage.getItem('url')) {
         url = window.sessionStorage.getItem('url');
     } 
