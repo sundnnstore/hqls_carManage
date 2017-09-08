1cc081232703020f0cdc8eb20a42395706a51d8f
fujl <215849803@qq.com>
2017-09-08 16:05:16 +0800

    update

--- src/main/java/com/sinoauto/dto/FlowListDto.java
+++ src/main/java/com/sinoauto/dto/FlowListDto.java
@@ -16,8 +16,14 @@ public class FlowListDto {
 	@ApiModelProperty("余额")
 	private Double balance = 0.00;
 
-	@ApiModelProperty("可提现")
-	private Double cashAble = 0.00;
+	@ApiModelProperty("本月已为您返现")
+	private Double currentReturned;
+
+	@ApiModelProperty("累计已返现")
+	private Double sumReturned;
+
+	@ApiModelProperty("剩余返现额")
+	private Double remainingReturned;
 
 	@ApiModelProperty("流水ID")
 	private List<FlowDto> flowList = new ArrayList<FlowDto>();
@@ -32,16 +38,6 @@ public class FlowListDto {
 		}
 	}
 
-	public Double getCashAble() {
-		return cashAble;
-	}
-
-	public void setCashAble(Double cashAble) {
-		if (cashAble != null) {
-			this.cashAble = cashAble;
-		}
-	}
-
 	public List<FlowDto> getFlowList() {
 		return flowList;
 	}
@@ -49,6 +45,36 @@ public class FlowListDto {
 	public void setFlowList(List<FlowDto> flowList) {
 		if (flowList != null) {
 			this.flowList = flowList;
+		}
+	}
+
+	public Double getCurrentReturned() {
+		return currentReturned;
+	}
+
+	public void setCurrentReturned(Double currentReturned) {
+		if (currentReturned != null) {
+			this.currentReturned = currentReturned;
+		}
+	}
+
+	public Double getSumReturned() {
+		return sumReturned;
+	}
+
+	public void setSumReturned(Double sumReturned) {
+		if (sumReturned != null) {
+			this.sumReturned = sumReturned;
+		}
+	}
+
+	public Double getRemainingReturned() {
+		return remainingReturned;
+	}
+
+	public void setRemainingReturned(Double remainingReturned) {
+		if (remainingReturned != null) {
+			this.remainingReturned = remainingReturned;
 		}
 	}
 
