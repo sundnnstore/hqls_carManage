ceebd92cb59d2b45e515c21453b4d390a1ea707f
fujl <215849803@qq.com>
2017-09-08 16:12:45 +0800

    添加返利

---
+++ src/main/java/com/sinoauto/dao/mapper/RebateMapper.java
@@ -0,0 +1,19 @@
+package com.sinoauto.dao.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface RebateMapper {
+
+	//select sum(rebate_money),is_rebate from hqls_rebate where store_id=1 group by is_rebate  ;
+	//select sum(rebate_money) from hqls_rebate where store_id = 1 and is_rebate = 1 and DATEDIFF(now(),rebate_time)=0;
+	
+	@Select("select sum(rebate_money) from hqls_rebate where store_id = #{storeId} and is_rebate = 1 and DATEDIFF(now(),rebate_time)=0;")
+	public Double getCurrentReturnedMoney(@Param("storeId") Integer storeId);
+	
+	@Select("select sum(rebate_money) from hqls_rebate where store_id = #{storeId} group by is_rebate order by is_rebate;")
+	public Double[] getRebateMoney(@Param("storeId") Integer storeId);
+
+}
