5d48af159b3f3f2a69d5a2a0dc16d923ef5db174
tang <tang@DESKTOP-R8A6IOT>
2017-09-19 14:07:18 +0800

    11

--- src/main/java/com/sinoauto/util/Constant.java
+++ src/main/java/com/sinoauto/util/Constant.java
@@ -2,9 +2,9 @@ package com.sinoauto.util;
 
 public class Constant {
 	// 极光推送的MASTER_SECRET
-	public static final String MASTER_SECRET = "aa7a69f76c6540d29af2936e";
+	public static final String MASTER_SECRET = "b7a1b9adf15d3e47a701b407";
 	// 极光推送APP_KEY
-	public static final String APP_KEY = "492198b64d68e0166d23dad7";
+	public static final String APP_KEY = "3638189b445792e8025a3c4c";
 	//APNS生产环境
 	public static final Boolean APNS_PRODUCTION = false;
 	//登录使用的同意UUID
