5c7f4496ca6dedf899f20c7644ed17d07a15f770
Administrator <Administrator@SC-201711092140>
2017-11-14 15:45:05 +0800

    提交

--- src/main/java/com/sinoauto/service/c/ProjectService.java
+++ src/main/java/com/sinoauto/service/c/ProjectService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.sinoauto.dao.bean.c.CBanner;
 import com.sinoauto.dao.bean.c.CCustomer;
